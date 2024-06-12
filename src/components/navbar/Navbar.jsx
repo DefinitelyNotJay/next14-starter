23ede4576924a5795b7252116f7eeebcb17e89d2
Tendou26 <jjbrathers3@gmail.com>
2024-06-12 21:03:10 +0700

    change

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import React from "react";
+import Links from "./links/Links";
+import styles from "./navbar.module.css"
+
+const NavBar = () => {
+  return (
+    <div className={styles.container}>
+        <div className={styles.icon}>Logo</div>
+      <Links />
+    </div>
+  );
+};
+
+export default NavBar;
